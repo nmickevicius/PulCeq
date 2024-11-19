d70ea077800a1a6db319668097c7c7eb73672c4a
nmickevicius <nikolaimickevicius@gmail.com>
2024-11-19 08:37:22 -0600

    include changing of directories in the pulseqtotoppe function

--- matlab/pulseqtotoppe.m
+++ matlab/pulseqtotoppe.m
@@ -1,5 +1,10 @@
 function pulseqtotoppe(seqName, tarName, maxView, maxGrad, maxSlew, maxRF, rfDeadTime, rfRingdownTime, adcDeadTime)
 
+[seqdir,name,ext] = fileparts(seqName);
+olddir = cd(seqdir);
+
 sysGE = toppe.systemspecs('maxGrad', maxGrad, 'maxSlew', maxSlew, 'maxView', maxView, 'maxRF', maxRF, ...
 'rfDeadTime', rfDeadTime, 'rfRingdownTime', rfRingdownTime, 'adcDeadTime', adcDeadTime);
 seq2ge_mcw(seqName, sysGE, tarName, true);
+
+~ = cd(olddir);
